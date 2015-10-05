345a3518653a8171577e1e9fff4faa98e1bcc3c5
Luis Capelo <luiscape@gmail.com>
2015-10-05 13:38:39 -0400

    installing pip

--- bin/setupsw.sh
+++ bin/setupsw.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# virtualenv venv
+#
+# Installing virtual environment.
+# And the latest version of pip.
+#
+virtualenv venv
+pip install --upgrade pip
 source venv/bin/activate
 
+#
+# Installing Python requirements
+# and running collector configuration.
+#
 pip install -r tool/requirements.txt
 python tool/scripts/config/
